90ed9af9c8a78721073b362c554c13516168a4d7
Loïc <loic.dusseux@2isa.org>
2016-09-12 15:13:20 +0200

    PREMIER JET DE DE

---
+++ JEU2DES/Joueur.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JEU2DES
+{
+    class Joueur
+    {
+        #region Champs et properties
+        #endregion
+
+        #region Constructeurs
+        #endregion
+
+        #region Méthodes
+
+        #region Méthodes héritées et substituées
+        #endregion
+
+        #region Méthodes à implementer pour les interfaces
+        #endregion
+
+        #endregion
+    }
+}
