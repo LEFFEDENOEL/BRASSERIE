bfc1f0b0fbf421854b4c4094ec71dc94287ddb88
Laurence <laurence.serrano@2isa.org>
2016-09-12 17:11:10 +0200

    PRECODAGE CLASSE Jeu2Des

---
+++ JEU2DES/Jeu2Des.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JEU2DES
+{
+    class Jeu2Des
+    {
+        private De _De1;
+        private De _De2;
+        private De _Valeur;
+        private Classement _Classement;
+        private Joueur _Nom;
+        private Joueur _Score;
+
+        static int gagne = 1;
+        static int perdu = 1;
+        static int nombreLancer = 0;
+        static int point = 0;
+
+        public Jeu2Des(De de1, De De2, De valeur, Classement classement, Joueur nom, Joueur score)
+        {
+            _De1 = de1;
+            _De2 = De2;
+            _Valeur = valeur;
+            _Classement = classement;
+            _Nom = nom;
+            _Score = score;
+        }
+        public Jeu2Des(){}
+
+        public void JouerPartie()
+        {
+            nombreLancer = _De1 + _De2;
+            int n;
+            n = 11;
+            for(nombreLancer=0; nombreLancer<n; n++)
+            {
+
+            }
+            
+            if(nombreLancer > 10)
+            {
+
+            }
+        }
+
+        public void VoirClassement()
+        {
+
+        }
+    }
+}
