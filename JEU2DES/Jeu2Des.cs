ac3a8a4b1969435514425fc05803d8a71698426e
Laurence <laurence.serrano@2isa.org>
2016-09-12 17:40:51 +0200

    PRECODAGE CLASSE Jeu2Des

--- JEU2DES/Jeu2Des.cs
+++ JEU2DES/Jeu2Des.cs
@@ -8,48 +8,48 @@ namespace JEU2DES
 {
     public class Jeu2Des
     {
-        private De _De1;
-        private De _De2;
-        private De _Valeur;
-        private Classement _Classement;
-        private Joueur _Nom;
-        private Joueur _Score;
+        //private De _De1;
+        //private De _De2;
+        //private De _Valeur;
+        //private Classement _Classement;
+        //private Joueur _Nom;
+        //private Joueur _Score;
 
-        static int gagne = 1;
-        static int perdu = 1;
-        static int nombreLancer = 0;
-        static int point = 0;
+        //static int gagne = 1;
+        //static int perdu = 1;
+        //static int nombreLancer = 0;
+        //static int point = 0;
 
-        public Jeu2Des(De de1, De De2, De valeur, Classement classement, Joueur nom, Joueur score)
-        {
-            _De1 = de1;
-            _De2 = De2;
-            _Valeur = valeur;
-            _Classement = classement;
-            _Nom = nom;
-            _Score = score;
-        }
-        public Jeu2Des(){}
+        //public Jeu2Des(De de1, De De2, De valeur, Classement classement, Joueur nom, Joueur score)
+        //{
+        //    _De1 = de1;
+        //    _De2 = De2;
+        //    _Valeur = valeur;
+        //    _Classement = classement;
+        //    _Nom = nom;
+        //    _Score = score;
+        //}
+        //public Jeu2Des(){}
 
-        public void JouerPartie()
-        {
-            nombreLancer = _De1 + _De2;
-            int n;
-            n = 11;
-            for(nombreLancer=0; nombreLancer<n; n++)
-            {
+        //public void JouerPartie()
+        //{
+        //    nombreLancer = _De1 + _De2;
+        //    int n;
+        //    n = 11;
+        //    for(nombreLancer=0; nombreLancer<n; n++)
+        //    {
 
-            }
+        //    }
             
-            if(nombreLancer > 10)
-            {
+        //    if(nombreLancer > 10)
+        //    {
 
-            }
-        }
+        //    }
+        //}
 
-        public void VoirClassement()
-        {
+        //public void VoirClassement()
+        //{
 
-        }
+        //}
     }
 }
