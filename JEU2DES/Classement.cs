8fc45f5421f88d28cf9458612c4e77a1a0e34ebd
Laurent <laurent.faure@2isa.org>
2016-09-12 15:36:49 +0200

    CLASSE CLASSEMENT + CLASSE ENTREE
    Methodes à coder dans classe Entree

--- JEU2DES/Classement.cs
+++ JEU2DES/Classement.cs
@@ -8,7 +8,62 @@ namespace JEU2DES
 {
     public class Classement
     {
-        //TODO
-        //TODO3
+        private class Entree
+        {
+            #region Champs et Propriétés
+            private string _Nom;
+
+            public string Nom
+            {
+                get { return _Nom}
+                set { _Nom = value; }
+            }
+
+            private int _Score;
+            public int Score
+            {
+                get { return _Score}
+                set { _Score = value; }
+            }
+            #endregion
+
+            #region Constructeurs
+
+            //Constructeur par defaut
+            public Entree() { }
+
+            //Constructeur complet
+            public Entree(Classement classement, string nom, int score)
+            {
+                Nom = nom;
+                Score = score;
+            }
+            #endregion
+
+
+            #region Methodes
+
+            public bool AjouterEntree(string nom, int score)
+            {
+                //TODO
+                return true;
+            }
+
+            public void TopN()
+            {
+                //TODO
+            }
+
+            #endregion
+
+            #region Methodes héritées et substituées
+
+            public override string ToString()
+            {
+                return "Nom du joueur : " + Nom + "Score est égal à : " + Score;
+            }
+
+            #endregion
+        }
     }
 }
