ebba0cd42e2490385ce8c94ed343aeb4470d4088
Laurent <laurent.faure@2isa.org>
2016-09-15 15:41:25 +0200

    PRECODAGE FACTORY

---
+++ JEU2DES_V2/Factory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JEU2DES_V2
+{
+    public abstract class Factory
+    {
+        public static IStrategyPersistable IStrategyPersistable(int choice)
+        {
+            IStrategyPersistable objPersistable = null;
+
+            switch (choice)
+            {
+                //Binary case
+                case 1:
+                    objPersistable = new FichierBinaireStrategy();
+                    break;
+                //XML case
+                case 2:
+                    objPersistable = new FichierXmlStrategy();
+                    break;
+            }
+            return objPersistable;
+        }
+    }
+}
